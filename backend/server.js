345d2821bc22ae5334e45f7ea27ae0314202a97b
NezirNezirevic01 <70522081+NezirNezirevic01@users.noreply.github.com>
2022-01-09 15:10:49 +0100

    commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import dotenv from "dotenv";
+import productRouter from "./routers/productRouter.js";
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MOGNODB_URL || "mongodb://localhost/webshop", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("Serve at http://localhost:5000");
+});
